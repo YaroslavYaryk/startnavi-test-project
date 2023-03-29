93c62a2db4aacbda43dd2d63dab11dd2d3562259
YaroslavYaryk <duhanov2003@gmail.com>
2023-03-29 23:13:12 +0300

    readme

--- social_network/users/api/urls.py
+++ social_network/users/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import (
-    TokenRefreshView, TokenVerifyView, TokenObtainPairView
+    TokenRefreshView, TokenVerifyView
 )
 
 from .views import CreateUserAPIView, CustomTokenObtainPairView, UserApiView, UserActivityApiView
